17f5ef123899a4a452d8a5431736a6861be84e1b
sunnyvardhan02 <sunnyvardhan02@gmail.com>
2024-02-01 21:31:53 +0530

    FINAL

--- WEEK-1-TASK/index.js
+++ WEEK-1-TASK/index.js
@@ -83,3 +83,25 @@ document.addEventListener('DOMContentLoaded', function () {
       localStorage.setItem('tasks', JSON.stringify(tasks));
     }
   });
+
+
+
+  const express = require('express')
+const mongoose = require('mongoose')
+
+const routes = require('./routes/ToDoRoute')
+
+require('dotenv').config()
+
+ const app = express()
+
+const PORT = process.env.port || 5000
+
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log('Connected To MongoDB...'))
+    .catch((err) => console.log(err))
+
+   app.use(routes)
+   
+    app.listen(PORT, () => console.log('Listening on: $(PORT}'))
